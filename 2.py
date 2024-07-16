3e0e3bf67ba6b15472d6d4329a7fc3e418dd2dd5
nastialyash <user251@school22.rv.ua>
2024-07-16 21:12:25 +0300

    first commit

---
+++ 2.py
@@ -0,0 +1,45 @@
+# file1 = "file1.txt"
+# file2 = "file2.txt"
+# with open(file1,"r") as fileobg:
+#     data1 = fileobg.readlines()
+
+# with open(file2,"r") as fileobg2:
+#     data2 = fileobg2.readlines()   
+# length = max(len(data2),len(data1))
+# list = []
+
+
+# file1 = "file1.txt"
+# file2 = "file2.txt"
+# with open(file1,"r") as fileobg:
+#      with open(file1,"r") as fileobg:
+#         data1 = fileobg.readlines()   
+# lines = data1[:-1]
+
+# with open(file2,"r") as fileobg:
+#      with open(file2,"w") as fileobg:
+#          fileobg.writelines(data1)
+
+# print(file2)         
+
+# with open("text.txt","r") as file:
+#     data = file.readlines()
+
+# a = input("Enter your word : ")
+# count = 0
+# with open("text.txt","r") as file:
+#     data = file.read()
+# for i in data.split():
+#     count += i
+
+# print(count)        
+
+# a = input("Enter your nymber")
+# b = input("Enter your serch number")
+# with open("text.txt","r") as file:
+#     data = file.read()
+# data2 = data.replace(a,b)   
+# with open("text2.txt","w") as file:
+#     file.write(data2)
+# print(a,b)
+    
